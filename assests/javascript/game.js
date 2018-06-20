c981c9b1368387c773124b347615148d09f7fe16
Brad Fletcher <BradFletcher@NewUsers-MacBook-Air.local>
2018-06-19 18:37:58 -0600

    update

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -40,12 +40,14 @@ $("#crystal2").on("click", function() {
     total += diamondValue;
     // checkForWinOrLoss();
     console.log(total);
+    console.log('total variable:', total);
+    console.log('totalScore variable:', totalScore);
     $("#totalScore").html(total);
-    if (totalScore > randomNumber) {
+    if (total> randomNumber) {
         losses++
         youLose();
      }
-     else if (totalScore === randomNumber) {
+     else if (total === randomNumber) {
              wins++
              youWin();
         
@@ -56,11 +58,11 @@ $("#crystal1").on("click", function() {
     // checkForWinOrLoss();
     console.log(total);
     $("#totalScore").html(total);
-    if (totalScore > randomNumber) {
+    if (total > randomNumber) {
         losses++
         youLose();
      }
-     else if (totalScore === randomNumber) {
+     else if (total === randomNumber) {
              wins++
              youWin();
         
@@ -72,11 +74,11 @@ $("#crystal3").on("click", function() {
   
     console.log(total)
     $("#totalScore").html(total);
-    if (totalScore > randomNumber) {
+    if (total > randomNumber) {
         losses++
         youLose();
      }
-     else if (totalScore === randomNumber) {
+     else if (total === randomNumber) {
              wins++
              youWin();
         
@@ -87,11 +89,11 @@ $("#crystal4").on("click", function() {
     // checkForWinOrLoss();
     console.log(total)
     $("#totalScore").html(total);
-    if (totalScore > randomNumber) {
+    if (total > randomNumber) {
         losses++
         youLose();
      }
-     else if (totalScore === randomNumber) {
+     else if (total === randomNumber) {
              wins++
              youWin();
         
@@ -99,10 +101,15 @@ $("#crystal4").on("click", function() {
 })
 
 function newGame() {
-    randomNumberFromRange = 0;
-    totalScore = 0;
+    randomNumber = randomNumberFromRange(19, 120);
+    diamondValue = randomNumberFromRange(1, 12);
+    triangleValue = randomNumberFromRange(1, 12);
+    threeValue = randomNumberFromRange(1, 12);
+     hexValue = randomNumberFromRange(1, 12);
+
+    total = 0;
     $("#randomNumber").text(randomNumber);
-    $("#totalScore").text(totalScore);
+    $("#totalScore").text(total);
     $("#crystal1").attr( diamondValue);
     $("#crystal2").attr( triangleValue);
     $("#crystal3").attr(threeValue);
@@ -179,3 +186,101 @@ function youLose() {
 // }
 
 
+
+
+
+//     <button onclick="myFunction()">Click me</button>
+
+//     <script>
+// function myFunction() {
+//   document.getElementById("demo").innerHTML = "Hello World";
+// }
+// </script>
+
+// or document.querySelector("#gameID").innerHTML = htmlGame;
+
+// var computerRandom = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+    
+
+
+// if (guess === computerRandom) {
+//   wins++;
+//   won = true;
+//   tries=9;
+// }
+// if (guessesSoFar == []) {
+//   guessesSoFar = guess;
+// } 
+// else {
+//   guessesSoFar = guessesSoFar + "," + guess;
+// }
+// tries++;
+// guessLeft--;
+
+
+// if (tries > 8) {
+//  guessLeft = 9;
+//  guessesSoFar = "";
+//  tries = 0;
+    
+//  if (won === false) {
+//    losses++;
+//  } 
+//  else {
+//    won = false;
+//  }
+// }
+
+// //    * The player wins if their total score matches the random number from the beginning of the game.
+
+// if (playerClicks === computerRandom) {
+//     wins++;
+//     won = true;
+//     tries=9;
+//   }
+
+
+// //    * The player loses if their score goes above the random number.
+
+// if (playerClicks === computerRandom) {
+//     wins++;
+//     won = true;
+//     tries=9;
+//   }
+// //    * The game restarts whenever the player wins or loses.
+
+//     //  * When the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values. Of course, the user's score (and score counter) will reset to zero.
+
+// //    * The app should show the number of games the player wins and loses. To that end, do not refresh the page as a means to restart the game.
+
+// var htmlGame =
+//   "<p>Wins " + wins + "</p>" + 
+//   "<p>Losses " + losses + "</p>";
+
+// document.querySelector("#gameID").innerHTML = htmlGame;
+// }
+
+
+//  //  * When the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values. Of course, the user's score (and score counter) will reset to zero.
+
+
+// // * Each crystal should have a random hidden value between 1 - 12.
+
+// var minNumber = 1;
+// var maxNumber = 12;
+
+// var randomNumber = randomNumberFromRange(minNumber, maxNumber);
+
+// function randomNumberFromRange(min,max)
+// {
+//     return Math.floor(Math.random()*(max-min+1)+min);
+// }
+
+// var htmlGame =
+//   "<p>Wins " + wins + "</p>" + 
+//   "<p>Losses " + losses + "</p>" +
+// //   "<h3>"randomNumber" + "randomNumberFromRange "</h3>" 
+//   "<p>Your guesses so far " + guessesSoFar + "</p>";
+
+// document.querySelector("#gameID").innerHTML = htmlGame;
